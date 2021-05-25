30628a3743755d31fe89770ba670ca81f5dff12a
timperello <74350893+timperello@users.noreply.github.com>
2021-05-25 12:31:22 +0200

    zombie qui court
    le zombie court et s'arrête quand il est proche du joueur

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float stoppingDistance;
+
+    NavMeshAgent agent;
+
+    GameObject target;
+
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        float dist = Vector3.Distance(transform.position, target.transform.position);
+        if(dist < 2.5)
+        {
+            EnemyAttack();
+        }
+        else
+        {
+            GoToTarget();
+        }
+
+    }
+
+    private void GoToTarget()
+    {
+        agent.isStopped = false;
+        agent.SetDestination(target.transform.position);
+    }
+
+    private void EnemyAttack()
+    {
+        agent.isStopped = true;
+    }
+
+}
