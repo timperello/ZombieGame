5dab873029235ca5094a1d1782fa8b8c5a1bbeb9
Clement Palumbo <84730729+DarkAnakin69@users.noreply.github.com>
2021-05-25 18:20:21 +0200

    Ath barre de vie + munitions

---
+++ Assets/Munitions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Munitions : MonoBehaviour
+{
+    public int munitions = 30;
+    public int chargeur;
+    public bool tire;
+    public Text affichageMunitions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        affichageMunitions.text = munitions.ToString()+"/"+""+chargeur*30;
+        
+        if(munitions !=0 )
+        {
+            if (Input.GetMouseButtonDown(0) && !tire && munitions > 0)
+            {
+                tire = true;
+                munitions--;
+                tire = false;
+            }
+        }
+        if(chargeur !=0)
+        {
+            if (Input.GetMouseButtonDown(1)/* && appuye sur recharge*/)
+            {
+                chargeur--;
+                munitions = 30;
+            }
+        }
+        
+        
+    }
+}
