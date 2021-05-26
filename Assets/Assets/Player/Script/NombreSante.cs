089346947dad1c65260ee6e3ab7d874e0fe1e574
ElliotClnt <84764861+ElliotClnt74@users.noreply.github.com>
2021-05-26 17:01:14 +0200

    Mini map + Heal

---
+++ Assets/Assets/Player/Script/NombreSante.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NombreSante : MonoBehaviour
+{
+    public Text affichageNombreSante;
+    // Start is called before the first frame update
+    
+    public void SetHealth(int health)
+    {
+        affichageNombreSante.text = ""+ health +"";
+    }
+}
---
+++ Assets/Assets/Zombie/Script/Wave.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject zombie;
+    Vector3 posSpawn;
+    int rand;
+    private bool isCoroutineExecuting;
+    void Start()
+    {       
+        zombie = GameObject.Find("Enemy");       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(ExecuteAfterTime(5));
+        
+    }
+    void SpawnZombie()
+    {
+        rand = Random.Range(1, 7);
+        posSpawn = GameObject.Find("SpawnZombie" + rand).transform.position;
+        Instantiate(zombie,posSpawn, Quaternion.identity);        
+    }
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        if (isCoroutineExecuting)
+            yield break;
+
+        isCoroutineExecuting = true;
+
+        yield return new WaitForSeconds(time);
+
+        SpawnZombie();
+
+        isCoroutineExecuting = false;
+    }
+}
